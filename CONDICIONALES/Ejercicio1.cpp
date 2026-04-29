ffc1ee2741abc763c212dbc5175f6684ff304c1b
danielrivas10 <00071126@uca.edu.sv>
2026-04-28 18:55:12 -0600

    git commit -m " :memo: Actualización de información"

---
+++ CONDICIONALES/Ejercicio1.cpp
@@ -0,0 +1,40 @@
+//Escribe un programa que pida la edad de una persona 
+// Indique si es mayor o menor de edad (18 años o más es mayor de edad)
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+int edad;
+
+cout << "Bienvenido al programa para detectar tu edad" << endl;
+cout << "Por favor ingresa tu edad" << endl;
+cin >> edad;
+
+if (edad <0)
+{
+cout << "Usted tiene : " << edad << " años. Por lo que su edad no es valida" << endl ;
+
+}
+
+else if (edad <18)
+{
+
+cout << "Usted tiene :" << edad << " años. Por lo que es menor de edad" << endl;
+
+}
+
+else {
+
+cout << "Usted tiene :" << edad << " años. Por lo que es mayor de edad" << endl;
+
+}
+
+
+return 0;
+
+}
+
+
