b7818728873cb58128abff321689b1b1c05641e1
danielrivas10 <00071126@uca.edu.sv>
2026-05-06 12:37:53 -0600

    git commit -m ":memo: subiendo datos"

--- SWITCH/NivelDificil/ejercicio16.cpp
+++ SWITCH/NivelDificil/ejercicio16.cpp
@@ -3,32 +3,97 @@
 
 #include <iostream>
 using namespace std;
-#include <windows.h>
+
 
 int main()
 {
 
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
+    
 
-    string día;
+    int dia;
 
     cout << "Buen día usuario" << "Este programa es de ayuda para recordar su horario de clases" << endl;
     cout << "Podrías por favor seleccionar el día de la semana que te corrresponde" << endl;
     cout << "1. Lunes" << endl;
-    cout << "2. Lunes" << endl;
-    cout << "3. Lunes" << endl;
-    cout << "4. Lunes" << endl;
-    cout << "5. Lunes" << endl;
-    cout << "6. Lunes" << endl;
-    cout << "7. Lunes" << endl;
+    cout << "2. Martes" << endl;
+    cout << "3. Miercoles" << endl;
+    cout << "4. Jueves" << endl;
+    cout << "5. Viernes" << endl;
+    cout << "6. Sabado" << endl;
+    cout << "7. Domingo" << endl;
+    cin >> dia;
 
-switch(día){
+
+
+
+switch(dia) {
+
+
+case 1: 
+cout << "Tu horario es:" << endl;
+cout << "Precálculo 7:00 AM:" << endl;
+cout << "Lenguaje 9:00 AM:" << endl;
+cout << "Historia 11:00 AM:" << endl;
+
+break;
+
+case 2: 
+cout << "Tu horario es:" << endl;
+cout << "Sociales 7:00 AM:" << endl;
+cout << "Fisica 9:00 AM:" << endl;
+cout << "Historia 11:00 AM:" << endl;
+
+break;
+
+case 3: 
+cout << "Tu horario es:" << endl;
+cout << "Precálculo 7:00 AM:" << endl;
+cout << "Lenguaje 9:00 AM:" << endl;
+cout << "Historia 11:00 AM:" << endl;
+
+break;
+
+case 4: 
+cout << "Tu horario es:" << endl;
+cout << "Precálculo 7:00 AM:" << endl;
+cout << "Lenguaje 9:00 AM:" << endl;
+cout << "Quimica 11:00 AM:" << endl;
+
+break;
+
+case 5: 
+cout << "Tu horario es:" << endl;
+cout << "Precálculo 7:00 AM:" << endl;
+cout << "Lenguaje 9:00 AM:" << endl;
+cout << "Historia 11:00 AM:" << endl;
+
+break;
+
+case 6: 
+cout << "Tu horario es:" << endl;
+cout << "Ingles 7:00 AM:" << endl;
+cout << "Programación 9:00 AM:" << endl;
+
+break;
+
+case 7: 
+cout << "Este día descansas" << endl;
+
+break;
+
+default:
+cout <<"Por favor ingrese un numero del 1-7" << endl;
+
+break;
+
+
+
 
     
 }
 
+return 0;
 
-
-    return 0;
 }
+
+//Hecho
---
+++ SWITCH/NivelDificil/ejercicio17.cpp
@@ -0,0 +1,3 @@
+//Sistema de Transporte Público
+//Pide un tipo de transporte (1 = Bus, 2 = Metro, 3 = Taxi) y muestra el costo estimado de un viaje.
+
