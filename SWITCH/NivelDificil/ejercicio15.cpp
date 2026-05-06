dcefb0fc9bb3d60eb8b3b69f4ec1112ce6c49f5a
danielrivas10 <00071126@uca.edu.sv>
2026-05-06 12:14:32 -0600

    git commit -m ":memo: subiendo ejercicios"

--- SWITCH/NivelDificil/ejercicio15.cpp
+++ SWITCH/NivelDificil/ejercicio15.cpp
@@ -1,25 +1,75 @@
-//Cálculo de Impuestos
-// Solicita el salario de un trabajador y muestra el porcentaje de impuestos a pagar según su rango de ingresos.
-
+// Cálculo de Impuestos
+//  Solicita el salario de un trabajador y muestra el porcentaje de impuestos a pagar según su rango de ingresos.
 
 #include <iostream>
 using namespace std;
+#include <windows.h>
 
-int main ()
+int main()
 {
-    
 
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    int salario, opcion = 0;
 
+    // int rango = salario / 1000;
+    cout << "Buen día" << "Por favor ingrese su salario" << endl;
+    cout << "En base a su salario se mostrará su porcentaje de impuestos a pagar" << endl;
+    cout << "Por favor ingrese su salario" << endl;
+    cin >> salario;
 
-
-
-
-
-
-
-
-
-
-    
-return 0;
+    if (salario > 0 && salario < 1000)
+    {
+        opcion = 1;
     }
+
+    else if (salario > 1000 && salario < 2000)
+    {
+
+        opcion = 2;
+    }
+
+    else if (salario > 2000 && salario < 3000)
+    {
+
+        opcion = 3;
+    }
+
+    else if (salario > 3000 && salario < 4000)
+    {
+
+        opcion = 4;
+    }
+
+    switch (opcion)
+    {
+    case 1:
+
+        cout << "Su salario es de: " << salario << "Por lo que su porcentaje de impuestos a pagar es de 0% " << endl;
+
+        break;
+
+    case 2:
+
+        cout << "Su salario es de: " << salario << "Por lo que su porcentaje de impuestos a pagar es de 10% " << endl;
+
+        break;
+
+    case 3:
+
+        cout << "Su salario es de: " << salario << "Por lo que su porcentaje de impuestos a pagar es de 20% " << endl;
+
+        break;
+
+    case 4:
+
+        cout << "Su salario es de: " << salario << "Por lo que su porcentaje de impuestos a pagar es de 30% " << endl;
+
+    default:
+        cout << "Su salario es invalido" << endl;
+
+        break;
+    }
+
+    return 0;
+}
---
+++ SWITCH/NivelDificil/ejercicio16.cpp
@@ -0,0 +1,34 @@
+// Horario de Clases
+// Pide el día de la semana y muestra el horario de clases para ese día.
+
+#include <iostream>
+using namespace std;
+#include <windows.h>
+
+int main()
+{
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    string día;
+
+    cout << "Buen día usuario" << "Este programa es de ayuda para recordar su horario de clases" << endl;
+    cout << "Podrías por favor seleccionar el día de la semana que te corrresponde" << endl;
+    cout << "1. Lunes" << endl;
+    cout << "2. Lunes" << endl;
+    cout << "3. Lunes" << endl;
+    cout << "4. Lunes" << endl;
+    cout << "5. Lunes" << endl;
+    cout << "6. Lunes" << endl;
+    cout << "7. Lunes" << endl;
+
+switch(día){
+
+    
+}
+
+
+
+    return 0;
+}
